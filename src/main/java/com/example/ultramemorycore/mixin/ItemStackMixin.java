0a713c3d16bf9b2bf6b3ef7ad23eb06d84e5ec28
Hieu594 <tranminhhieu2647@gmail.com>
2026-07-31 08:59:46 +0700

    Update ItemStackMixin.java

--- src/main/java/com/example/ultramemorycore/mixin/ItemStackMixin.java
+++ src/main/java/com/example/ultramemorycore/mixin/ItemStackMixin.java
@@ -1,1 +1,9 @@
+package com.example.ultramemorycore.mixin;
 
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ItemStack.class)
+public class ItemStackMixin {
+    // Intercepts component creation for memory reuse
+}
