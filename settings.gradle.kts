77b709845f929d3ca4c407b738d6247c6544aca5
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-03 16:33:32 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,7 @@
 pluginManagement {
     repositories {
-        maven("https://maven.fabricmc.net/") {
-            name = "Fabric"
-        }
-
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-
-    repositories {
-        // Fabric
         maven("https://maven.fabricmc.net/")
-
-        // Mod Menu
-        maven("https://maven.terraformersmc.com/releases/")
-
-        // Cloth Config
-        maven("https://maven.shedaniel.me/")
-
-        // Maven Central
+        gradlePluginPortal()
         mavenCentral()
     }
 }
