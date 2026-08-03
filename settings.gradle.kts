c6e9cfdf39d6ba2801769a607517a113b1697cc9
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-03 13:45:38 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,32 @@
+```kotlin
 pluginManagement {
     repositories {
-        maven("https://maven.fabricmc.net/") { name = "Fabric" }
+        maven("https://maven.fabricmc.net/") {
+            name = "Fabric"
+        }
+
         mavenCentral()
         gradlePluginPortal()
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
+    repositories {
+        // Fabric
+        maven("https://maven.fabricmc.net/")
+
+        // Mod Menu
+        maven("https://maven.terraformersmc.com/releases/")
+
+        // Cloth Config
+        maven("https://maven.shedaniel.me/")
+
+        // Maven Central
+        mavenCentral()
+    }
+}
+
 rootProject.name = "UltraMemoryCore"
+```
