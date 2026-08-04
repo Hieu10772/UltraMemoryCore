71aea656d4ca9e4308e0c9fdbdf00c1f976d31c3
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-05 06:02:29 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,14 +19,13 @@ repositories {
 
 dependencies {
     minecraft("com.mojang:minecraft:${property("minecraft_version")}")
+    mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
 
-    mappings(loom.officialMojangMappings())
+    modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
 
-    implementation("net.fabricmc:fabric-loader:${property("loader_version")}")
-    implementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
-
-    implementation("me.shedaniel.cloth:cloth-config-fabric:${property("cloth_config_version")}")
-    implementation("com.terraformersmc:modmenu:${property("modmenu_version")}")
+    modImplementation("me.shedaniel.cloth:cloth-config-fabric:${property("cloth_config_version")}")
+    modImplementation("com.terraformersmc:modmenu:${property("modmenu_version")}")
 
     // Explicit dependencies for standalone benchmark and future optimization code
     implementation("com.google.code.gson:gson:2.13.1")
