db890d6504ceed54ca165bac2f650ed413963805
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-04 21:54:49 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,18 +18,13 @@ repositories {
 }
 
 dependencies {
-    val minecraftVersion = project.property("minecraft_version") as String
-    val yarnMappings = project.property("yarn_mappings") as String
-    val loaderVersion = project.property("loader_version") as String
-    val fabricVersion = project.property("fabric_version") as String
-
     minecraft("com.mojang:minecraft:${property("minecraft_version")}")
-    mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
-    modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
 
-    modImplementation("me.shedaniel.cloth:cloth-config-fabric:${property("cloth_config_version")}")
-    modImplementation("com.terraformersmc:modmenu:${property("modmenu_version")}")
+    implementation("net.fabricmc:fabric-loader:${property("loader_version")}")
+    implementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_version")}")
+
+    implementation("me.shedaniel.cloth:cloth-config-fabric:${property("cloth_config_version")}")
+    implementation("com.terraformersmc:modmenu:${property("modmenu_version")}")
 
     // Explicit dependencies for standalone benchmark and future optimization code
     implementation("com.google.code.gson:gson:2.13.1")
