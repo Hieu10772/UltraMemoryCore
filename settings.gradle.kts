15436193cab9a1b77067d20d0761a5c3eeb7ac18
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-05 07:38:28 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,16 @@
 pluginManagement {
     repositories {
-        maven("https://maven.fabricmc.net/")
         gradlePluginPortal()
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.fabricmc.net/snapshots")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
         mavenCentral()
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.fabricmc.net/snapshots")
     }
 }
 
