145627b47d42f89e55c07333d3836c54f5c243ef
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-07 07:04:26 +0700

    Delete Common/src/main/java/com/example/ultramemorycore/mixin directory

--- Common/src/main/java/com/example/ultramemorycore/mixin/ChunkSectionMixin.java
+++
@@ -1,9 +0,0 @@
-package com.example.ultramemorycore.mixin;
-
-import net.minecraft.world.chunk.ChunkSection;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ChunkSection.class)
-public class ChunkSectionMixin {
-    // Hooks for chunk section memory optimization
-}
