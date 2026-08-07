77c69050ca1ba0fb91b3f854a6972f85b2af1e58
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-07 12:04:28 +0700

    Delete Common/src/main/java/com/example/ultramemorycore/mixin directory

--- Common/src/main/java/com/example/ultramemorycore/mixin/ItemStackMixin.java
+++
@@ -1,9 +0,0 @@
-package com.example.ultramemorycore.mixin;
-
-import net.minecraft.item.ItemStack;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ItemStack.class)
-public class ItemStackMixin {
-    // Intercepts component creation for memory reuse
-}
