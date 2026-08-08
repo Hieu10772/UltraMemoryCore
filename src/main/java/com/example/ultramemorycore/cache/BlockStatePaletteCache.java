4b65bab2245e0e19106c5946c0fa7b26355def5d
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-08 12:00:32 +0700

    Update BlockStatePaletteCache.java

--- src/main/java/com/example/ultramemorycore/cache/BlockStatePaletteCache.java
+++ src/main/java/com/example/ultramemorycore/cache/BlockStatePaletteCache.java
@@ -1,7 +1,7 @@
 package com.example.ultramemorycore.cache;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
-import net.minecraft.block.BlockState;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class BlockStatePaletteCache {
     private static final Int2ObjectOpenHashMap<BlockState> CACHE = new Int2ObjectOpenHashMap<>();
