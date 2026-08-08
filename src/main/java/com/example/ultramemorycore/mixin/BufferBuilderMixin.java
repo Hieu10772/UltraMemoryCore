f3c076b5cdf48854c3a3b63610334401bc6ceaf6
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-08 11:49:01 +0700

    Update BufferBuilderMixin.java

--- src/main/java/com/example/ultramemorycore/mixin/BufferBuilderMixin.java
+++ src/main/java/com/example/ultramemorycore/mixin/BufferBuilderMixin.java
@@ -1,6 +1,6 @@
 package com.example.ultramemorycore.mixin;
 
-import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.renderer.BufferBuilder;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(BufferBuilder.class)
