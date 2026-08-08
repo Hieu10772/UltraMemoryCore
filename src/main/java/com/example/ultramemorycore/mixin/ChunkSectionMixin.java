67fd395a63d19ae40057654bf99a605daa54cc26
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-08 11:49:58 +0700

    Update ChunkSectionMixin.java

--- src/main/java/com/example/ultramemorycore/mixin/ChunkSectionMixin.java
+++ src/main/java/com/example/ultramemorycore/mixin/ChunkSectionMixin.java
@@ -1,9 +1,9 @@
 package com.example.ultramemorycore.mixin;
 
-import net.minecraft.world.chunk.ChunkSection;
+import net.minecraft.world.level.chunk.LevelChunkSection;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(ChunkSection.class)
+@Mixin(LevelChunkSection.class)
 public class ChunkSectionMixin {
     // Hooks for chunk section memory optimization
 }
