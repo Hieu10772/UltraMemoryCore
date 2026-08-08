1755e67edb60ef6e5af60360dd012e6be6cca3db
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-08 12:43:30 +0700

    Update ChunkColdStorage.java

--- src/main/java/com/example/ultramemorycore/memory/ChunkColdStorage.java
+++ src/main/java/com/example/ultramemorycore/memory/ChunkColdStorage.java
@@ -28,7 +28,7 @@ public final class ChunkColdStorage {
 
     public static void markVisible(ChunkPos pos) {
         LAST_VISIBLE.put(
-                pss.toLong(),
+                ChunkPos.asLong(pos.x(), pos.z()),
                 System.currentTimeMillis()
         );
     }
