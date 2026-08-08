fb8ec99288cd301882d710b3aaf0f0a19fb91408
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-08 11:45:57 +0700

    Update NbtCompoundMixin.java

--- src/main/java/com/example/ultramemorycore/mixin/NbtCompoundMixin.java
+++ src/main/java/com/example/ultramemorycore/mixin/NbtCompoundMixin.java
@@ -1,13 +1,13 @@
 package com.example.ultramemorycore.mixin;
 
 import com.example.ultramemorycore.cache.NbtStringPool;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(NbtCompound.class)
+@Mixin(CompoundTag.class)
 public class NbtCompoundMixin {
     @ModifyVariable(method = "put", at = @At("HEAD"), argsOnly = true, require = 0)
     private String internNbtKey(String key) {
