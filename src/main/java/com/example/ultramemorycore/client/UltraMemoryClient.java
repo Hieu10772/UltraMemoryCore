6b9bfbf683b06dad587db13c6f4af097331d3aa4
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-09 19:56:44 +0700

    Update UltraMemoryClient.java

--- src/main/java/com/example/ultramemorycore/client/UltraMemoryClient.java
+++ src/main/java/com/example/ultramemorycore/client/UltraMemoryClient.java
@@ -1,19 +1,21 @@
 package com.example.ultramemorycore.client;
 
 import com.example.ultramemorycore.memory.AdaptiveCacheManager;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-public final class UltraMemoryClient implements ClientModInitializer {
+@SideOnly(Side.CLIENT)
+@Mod.EventBusSubscriber(Side.CLIENT)
+public final class UltraMemoryClient {
 
-    @Override
-    public void onInitializeClient() {
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-
+    @SubscribeEvent
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        // Chỉ chạy ở cuối mỗi tick (tương đương END_CLIENT_TICK của Fabric)
+        if (event.phase == TickEvent.Phase.END) {
             AdaptiveCacheManager.tick();
-
-        });
-
+        }
     }
 }
