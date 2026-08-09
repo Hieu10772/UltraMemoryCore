4fe8c4d8cb7b65da1aaf6e5044dd870c789c91b1
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-09 20:20:50 +0700

    Update BufferBuilderMixin.java

--- src/main/java/com/example/ultramemorycore/mixin/BufferBuilderMixin.java
+++ src/main/java/com/example/ultramemorycore/mixin/BufferBuilderMixin.java
@@ -1,9 +1,9 @@
 package com.example.ultramemorycore.mixin;
 
-import net.minecraft.client.render.BufferBuilder;
+import net.minecraft.client.renderer.BufferBuilder;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(BufferBuilder.class)
 public class BufferBuilderMixin {
-    // Intercepts client buffer allocations to utilize UploadBufferPool
+    
 }
