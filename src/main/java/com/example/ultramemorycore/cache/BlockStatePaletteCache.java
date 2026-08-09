d74ee77f4b76468759c8b43aca8939eda6e09b89
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-09 09:28:21 +0700

    Update BlockStatePaletteCache.java

--- src/main/java/com/example/ultramemorycore/cache/BlockStatePaletteCache.java
+++ src/main/java/com/example/ultramemorycore/cache/BlockStatePaletteCache.java
@@ -1,16 +1,16 @@
 package com.example.ultramemorycore.cache;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.state.IBlockState;
 
 public class BlockStatePaletteCache {
-    private static final Int2ObjectOpenHashMap<BlockState> CACHE = new Int2ObjectOpenHashMap<>();
+    private static final Int2ObjectOpenHashMap<IBlockState> CACHE = new Int2ObjectOpenHashMap<>();
 
-    public static synchronized BlockState get(int id) {
+    public static synchronized IBlockState get(int id) {
         return CACHE.get(id);
     }
 
-    public static synchronized void put(int id, BlockState state) {
+    public static synchronized void put(int id, IBlockState state) {
         CACHE.put(id, state);
     }
 
