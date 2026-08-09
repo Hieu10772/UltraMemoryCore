1bafdf513a0e4d17decb4fc0eb7a855f0d4eabd3
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-09 20:22:56 +0700

    Update PalettedContainerMixin.java

--- src/main/java/com/example/ultramemorycore/mixin/PalettedContainerMixin.java
+++ src/main/java/com/example/ultramemorycore/mixin/PalettedContainerMixin.java
@@ -1,16 +1,10 @@
 package com.example.ultramemorycore.mixin;
 
 import com.example.ultramemorycore.pool.PaletteArrayPool;
-import net.minecraft.world.chunk.PalettedContainer;
+import net.minecraft.world.chunk.BlockStateContainer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PalettedContainer.class)
+@Mixin(BlockStateContainer.class)
 public class PalettedContainerMixin {
-    @Inject(method = "copy", at = @At("HEAD"), require = 0)
-    private void onCopy(CallbackInfoReturnable<?> cir) {
-        // Intercept container copying to use pooled array buffers
-    }
+
 }
