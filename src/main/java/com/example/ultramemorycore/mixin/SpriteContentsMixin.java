5b27fb3a8856ec907f72b5a0ee634e0ce7460802
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-11 16:01:36 +0700

    Create SpriteContentsMixin.java

---
+++ src/main/java/com/example/ultramemorycore/mixin/SpriteContentsMixin.java
@@ -0,0 +1,20 @@
+package com.ultramemorycore.mixin;
+
+import com.ultramemorycore.client.SpriteAtlasUnloader;
+import net.minecraft.client.texture.SpriteContents;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(SpriteContents.class)
+public class SpriteContentsMixin {
+
+    @Inject(
+        method = "upload",
+        at = @At("TAIL")
+    )
+    private void onUploadCompleted(CallbackInfo ci) {
+        SpriteAtlasUnloader.freeMipmapNativeMemory((SpriteContents) (Object) this);
+    }
+}
