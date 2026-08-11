b21fc4e260cd4a464c7d44ee6551d8993707d4d4
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-11 18:56:47 +0700

    Update SpriteContentsMixin.java

--- src/main/java/com/example/ultramemorycore/mixin/SpriteContentsMixin.java
+++ src/main/java/com/example/ultramemorycore/mixin/SpriteContentsMixin.java
@@ -1,6 +1,6 @@
 package com.example.ultramemorycore.mixin;
 
-import com.ultramemorycore.client.SpriteAtlasUnloader;
+import com.example.ultramemorycore.client.SpriteAtlasUnloader;
 import net.minecraft.client.texture.SpriteContents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
