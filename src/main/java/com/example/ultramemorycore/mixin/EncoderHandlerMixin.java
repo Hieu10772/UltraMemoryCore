526047dbc7bf841970bc3b18e158c31571a8ac1f
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-12 07:46:07 +0700

    Update EncoderHandlerMixin.java

--- src/main/java/com/example/ultramemorycore/mixin/EncoderHandlerMixin.java
+++ src/main/java/com/example/ultramemorycore/mixin/EncoderHandlerMixin.java
@@ -3,7 +3,7 @@ package com.example.ultramemorycore.mixin;
 import com.example.ultramemorycore.pool.NettyDirectBufferPool;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import net.minecraft.network.handler.EncoderHandler;
+import net.minecraft.network.PacketHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
