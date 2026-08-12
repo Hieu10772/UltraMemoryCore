9330bc750aaec378f493f043f70c2d4e02e5490a
Hieu10772 <tranminhhieu2647@gmail.com>
2026-08-12 08:01:50 +0700

    Update EncoderHandlerMixin.java

--- src/main/java/com/example/ultramemorycore/mixin/EncoderHandlerMixin.java
+++ src/main/java/com/example/ultramemorycore/mixin/EncoderHandlerMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(EncoderHandler.class)
+@Mixin(PacketEncoder.class)
 public class EncoderHandlerMixin {
 
     @Inject(
